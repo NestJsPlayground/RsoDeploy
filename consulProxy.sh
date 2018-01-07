68c417f740f793235543cf22b0ae03ccfd18b1d2
Tilen Tomakić <tilen@tomakic.com>
2018-01-07 16:30:50 +0100

    Update deploy script.

--- consulProxy.sh
+++ consulProxy.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-kubectl port-forward consul 8500:8500
+kubectl port-forward consul-0 8500:8500
+
--- deploy.sh
+++ deploy.sh
@@ -2,19 +2,23 @@
 
 gcloud container clusters get-credentials "fri-cluster" --zone "europe-west3-c"
 
-echo "Creating secret..."
-kubectl delete -f secret.yaml
-kubectl create -f secret.yaml
+if [[ $1 = "-s" ]]; then
+  echo "Creating secret..."
+  kubectl delete -f secret.yaml
+  kubectl create -f secret.yaml
+  exit 1;
+fi
 
 rm -rf /tmp/KUBERNETES_DEPLOY
 mkdir -p /tmp/KUBERNETES_DEPLOY
 VERSION=`date +'%Y-%m-%d %H:%M:%S'`
 
-if [ $1 = "del" ]; then
+if [[ $1 = "-d" ]]; then
   ACTION="delete"
 else
   ACTION="apply"
 fi
+echo "Action: $ACTION";
 
 PROJECT_DIR='/tmp/KUBERNETES_DEPLOY/seed'
 mkdir -p "$PROJECT_DIR"
@@ -26,6 +30,17 @@ kubectl "$ACTION" -f "$PROJECT_DIR/consul-service.yaml"
 kubectl "$ACTION" -f "$PROJECT_DIR/consul-statefulset.yaml"
 
 echo "Deploying seed..."
+kubectl "$ACTION" -f "$PROJECT_DIR/app-statefulset.yaml"
+kubectl "$ACTION" -f "$PROJECT_DIR/app-service.yaml"
+
+
+
+PROJECT_DIR='/tmp/KUBERNETES_DEPLOY/auth'
+mkdir -p "$PROJECT_DIR"
+cp ./rso-auth/kubernetes/* "$PROJECT_DIR/"
+sed -i -e "s/%DEPLOY_VERSION%/$VERSION/g" "$PROJECT_DIR/app-statefulset.yaml"
+
+echo "Deploying auth..."
 kubectl "$ACTION" -f "$PROJECT_DIR/app-statefulset.yaml"
 kubectl "$ACTION" -f "$PROJECT_DIR/app-service.yaml"
 
